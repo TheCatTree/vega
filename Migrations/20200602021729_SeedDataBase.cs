4f0c41b60aa90cee0ff41b3773ae37db128b15eb
TheCatTree <steven.c.hartley@gmail.com>
2020-06-09 12:58:12 +1200

    Adding Auto Mapper,and Mapping Resources

---
+++ Migrations/20200602021729_SeedDataBase.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace vega.Migrations
+{
+    public partial class SeedDataBase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Makes (Name) VALUES ('Make 1')");
+            migrationBuilder.Sql("INSERT INTO Makes (Name) VALUES ('Make 2')");
+            migrationBuilder.Sql("INSERT INTO Makes (Name) VALUES ('Make 3')");
+
+            migrationBuilder.Sql("INSERT INTO Models (Name, MakeID) VALUES ('Make-1 Model-A',(SELECT ID FROM Makes WHERE Name = 'Make 1'))");
+            migrationBuilder.Sql("INSERT INTO Models (Name, MakeID) VALUES ('Make-1 Model-B',(SELECT ID FROM Makes WHERE Name = 'Make 1'))");
+            migrationBuilder.Sql("INSERT INTO Models (Name, MakeID) VALUES ('Make-1 Model-C',(SELECT ID FROM Makes WHERE Name = 'Make 1'))");
+
+            migrationBuilder.Sql("INSERT INTO Models (Name, MakeID) VALUES ('Make-2 Model-A',(SELECT ID FROM Makes WHERE Name = 'Make 2'))");
+            migrationBuilder.Sql("INSERT INTO Models (Name, MakeID) VALUES ('Make-2 Model-B',(SELECT ID FROM Makes WHERE Name = 'Make 2'))");
+            migrationBuilder.Sql("INSERT INTO Models (Name, MakeID) VALUES ('Make-2 Model-C',(SELECT ID FROM Makes WHERE Name = 'Make 2'))");
+
+            migrationBuilder.Sql("INSERT INTO Models (Name, MakeID) VALUES ('Make-3 Model-A',(SELECT ID FROM Makes WHERE Name = 'Make 3'))");
+            migrationBuilder.Sql("INSERT INTO Models (Name, MakeID) VALUES ('Make-3 Model-B',(SELECT ID FROM Makes WHERE Name = 'Make 3'))");
+            migrationBuilder.Sql("INSERT INTO Models (Name, MakeID) VALUES ('Make-3 Model-C',(SELECT ID FROM Makes WHERE Name = 'Make 3'))");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Makes WHERE NAME IN ('Make 1', 'Make 2', 'Make 3')");
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -5,6 +5,9 @@ using Microsoft.AspNetCore.SpaServices.AngularCli;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using vega.Persistence;
+using AutoMapper;
 
 namespace vega
 {
@@ -20,12 +23,16 @@ namespace vega
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddAutoMapper(typeof(Startup).Assembly);
             services.AddControllersWithViews();
             // In production, the Angular files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
             {
                 configuration.RootPath = "ClientApp/dist";
             });
+
+            services.AddDbContext<VegaDbContext>(options =>
+            options.UseSqlServer(Configuration["ConnectionStrings:Default"]));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
